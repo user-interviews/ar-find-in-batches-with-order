7eeb04635ec5e177cb7b4dc6d66441fe1501fe83
Jeff Baxendale <jeffbax@gmail.com>
2021-06-10 14:11:51 -0400

    Clean up some of the code for legibility
    - bumps version to 0.1.0
    - fix typos
    - add frozen string comment
    - reduce nested ternary usage that makes it hard to follow
    - limit line length to 80 characters

--- lib/ar-find-in-batches-with-order.rb
+++ lib/ar-find-in-batches-with-order.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "ar-find-in-batches-with-order/version"
 
 module ActiveRecord
@@ -5,27 +7,38 @@ module ActiveRecord
     def find_in_batches_with_order(options = {})
       relation = self
 
-      # we have to be explicit about the options to ensure proper ordering and retrieval
+      # be explicit about the options to ensure proper ordering and retrieval
+      direction = options.delete(:direction) ||
+        (arel.orders.first.try(:ascending?) ? :asc : nil) ||
+        (arel.orders.first.try(:descending?) ? :desc : nil) || :desc
 
-      direction = options.delete(:direction) || (arel.orders.first.try(:ascending?) ? :asc : nil) || (arel.orders.first.try(:descending?) ? :desc : nil) || :desc
       start = options.delete(:start)
       batch_size = options.delete(:batch_size) || 1000
       with_start_ids = []
 
-      # try to deduct the property_key, but safer to specificy directly
-      property_key = options.delete(:property_key) || arel.orders.first.try(:value).try(:name) || arel.orders.first.try(:split,' ').try(:first)
-      tbl = connection.quote_table_name(options.delete(:property_table_name) || table.name)
+      # try to deduct the property_key, but safer to specify directly
+      property_key = options.delete(:property_key) ||
+        arel.orders.first.try(:value).try(:name) ||
+        arel.orders.first.try(:split,' ').try(:first)
+
+      table_name = options.delete(:property_table_name) || table.name
+      tbl = connection.quote_table_name(table_name)
       sanitized_key = "#{tbl}.#{connection.quote_column_name(property_key)}"
       relation = relation.limit(batch_size)
 
-      records = start ? (direction == :desc ? relation.where("#{sanitized_key} <= ?", start).to_a : relation.where("#{sanitized_key} >= ?", start).to_a)  : relation.to_a
+      records =
+        if !start
+          relation.to_a
+        elsif direction == :desc
+          relation.where("#{sanitized_key} <= ?", start).to_a
+        else
+          relation.where("#{sanitized_key} >= ?", start).to_a
+        end
 
       while records.any?
         records_size = records.size
 
         yield records
-
-
         break if records_size < batch_size
 
         next_start = records.last.try(property_key)
@@ -38,21 +51,25 @@ module ActiveRecord
           end
         end
 
-        without_dups = relation.where.not(relation.klass.primary_key => with_start_ids)
-        records = (direction == :desc ? without_dups.where("#{sanitized_key} <= ?", start).to_a : without_dups.where("#{sanitized_key} >= ?", start).to_a)
+        without_dups =
+          relation.where.not(relation.klass.primary_key => with_start_ids)
+
+        records =
+          if direction == :desc
+            without_dups.where("#{sanitized_key} <= ?", start).to_a
+          else
+            without_dups.where("#{sanitized_key} >= ?", start).to_a
+          end
       end
     end
 
     def find_each_with_order(options = {})
       find_in_batches_with_order(options) do |records|
-
         records.each do |record|
           yield record
         end
       end
     end
-
-
   end
 
   class Relation
--- lib/ar-find-in-batches-with-order/version.rb
+++ lib/ar-find-in-batches-with-order/version.rb
@@ -1,5 +1,5 @@
 module ActiveRecord
   module FindInBatchesWithOrder
-    VERSION = "0.0.2"
+    VERSION = "0.1.0"
   end
 end
